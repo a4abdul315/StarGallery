9aec2957055cd46bba39a2cf00439ee4cccba394
Abdul Rehman <a4abdul315@gmail.com>
2026-05-13 11:21:44 +0400

    RESTORE: Frontend perfectly synchronized and folder structure corrected

--- client/src/src/app/core/models/category.model.ts
+++
@@ -1,8 +0,0 @@
-export interface Category {
-  id: number;
-  name: string;
-  nameAr: string;
-  icon: string;     // PrimeIcon class name
-  image?: string;   // optional image URL
-  color: string;    // bg color for icon circle
-}
